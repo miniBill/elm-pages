04f9b007cbca171654abeab244f86d7bb60b7c8e
Leonardo Taglialegne <cmt.miniBill@gmail.com>
2024-08-27 09:45:23 +0200

    Add a flag to disable terser

--- generator/src/build.js
+++ generator/src/build.js
@@ -581,7 +581,7 @@ async function compileElm(options, config) {
   const patchedCode = patchFrozenViews(elmCode);
   await fsPromises.writeFile(fullOutputPath, patchedCode);
 
-  if (!options.debug) {
+  if (options.terser && !options.debug) {
     await runTerser(fullOutputPath);
   }
 
